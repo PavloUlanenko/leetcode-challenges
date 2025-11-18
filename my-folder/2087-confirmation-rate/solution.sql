9a429a84d0d778266cc82ae3ef885ee03b9e30c3
PavloUlanenko <39810676+PavloUlanenko@users.noreply.github.com>
2025-11-18 19:23:56 +0000

    [LeetCode Sync] Runtime - 241 ms (57.12%), Memory - 0.0B (100.00%)

---
+++ my-folder/2087-confirmation-rate/solution.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    s.user_id,
+    ROUND(AVG(COALESCE((c.action = 'confirmed')::int, 0)), 2) AS confirmation_rate
+FROM
+    Signups s
+LEFT JOIN
+    Confirmations c ON s.user_id = c.user_id
+GROUP BY
+    s.user_id;
