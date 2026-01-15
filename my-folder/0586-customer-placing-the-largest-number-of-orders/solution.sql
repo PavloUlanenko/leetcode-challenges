782a3dde3b8a5870fcd1752d7593db37f91ff07d
PavloUlanenko <39810676+PavloUlanenko@users.noreply.github.com>
2026-01-15 20:27:19 +0000

    [LeetCode Sync] Runtime - 234 ms (71.08%), Memory - 0.0B (100.00%)

---
+++ my-folder/0586-customer-placing-the-largest-number-of-orders/solution.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    customer_number
+    -- COUNT(order_number)
+FROM
+    Orders
+GROUP BY customer_number
+ORDER BY COUNT(order_number) DESC
+LIMIT 1;
