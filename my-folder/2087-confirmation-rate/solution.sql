080a3bf997e4448fa081d4cc84a8794891feed69
PavloUlanenko <39810676+PavloUlanenko@users.noreply.github.com>
2026-02-07 21:38:24 +0000

    [LeetCode Sync] Runtime - 244 ms (52.87%), Memory - 0.0B (100.00%)

--- my-folder/2087-confirmation-rate/solution.sql
+++ my-folder/2087-confirmation-rate/solution.sql
@@ -1,10 +1,7 @@
 -- Write your PostgreSQL query statement below
 SELECT
     s.user_id,
-    ROUND(AVG(COALESCE((c.action = 'confirmed')::int, 0)), 2) AS confirmation_rate
-FROM
-    Signups s
-LEFT JOIN
-    Confirmations c ON s.user_id = c.user_id
-GROUP BY
-    s.user_id;
+    ROUND(COALESCE(AVG(CASE WHEN c.action = 'timeout' OR c.action IS NULL THEN 0 ELSE 1 END), 0), 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id
