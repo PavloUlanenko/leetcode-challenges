a8f83100471e0fe883ca2d5bb7b526b9ab6b4e6f
PavloUlanenko <39810676+PavloUlanenko@users.noreply.github.com>
2026-08-26 20:15:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 60.8 MB (98.28%)

---
+++ my-folder/0203-remove-linked-list-elements/solution.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    const initial = new ListNode(null, head);
+    let cur = head;
+    let prev = initial;
+
+    while (cur) {
+        if (cur.val === val) {
+            prev.next = cur.next; 
+        } else {
+            prev = cur;
+        }
+        cur = cur.next;
+    }
+
+    return initial.next;
+};
