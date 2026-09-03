a5f01fe95c8d0ba93d8774bae0f6592788d5c31f
PavloUlanenko <39810676+PavloUlanenko@users.noreply.github.com>
2026-09-03 18:29:37 +0000

    [LeetCode Sync] Runtime - 3 ms (11.54%), Memory - 57.9 MB (7.69%)

---
+++ my-folder/2210-find-target-indices-after-sorting-array/solution.ts
@@ -0,0 +1,12 @@
+function targetIndices(nums: number[], target: number): number[] {
+    nums.sort((a, b) => a - b);
+    const res = [];
+
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] === target) {
+            res.push(i);
+        }
+    }
+
+    return res;
+};
