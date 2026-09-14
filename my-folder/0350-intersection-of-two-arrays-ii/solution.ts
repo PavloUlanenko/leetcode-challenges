ba66dab7116dfd1f4a6345cf1b736e4e259cd9c2
PavloUlanenko <39810676+PavloUlanenko@users.noreply.github.com>
2026-09-14 18:20:36 +0000

    [LeetCode Sync] Runtime - 6 ms (12.92%), Memory - 56.5 MB (74.16%)

---
+++ my-folder/0350-intersection-of-two-arrays-ii/solution.ts
@@ -0,0 +1,21 @@
+function intersect(nums1: number[], nums2: number[]): number[] {
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+    const res = [];
+    let p1 = 0;
+    let p2 = 0;
+
+    while (p1 < nums1.length && p2 < nums2.length) {
+        if (nums1[p1] === nums2[p2]) {
+            res.push(nums1[p1]);
+            p1++;
+            p2++;
+        } else if (nums1[p1] < nums2[p2]) {
+            p1++;
+        } else {
+            p2++;
+        }
+    }
+
+    return res;
+};
